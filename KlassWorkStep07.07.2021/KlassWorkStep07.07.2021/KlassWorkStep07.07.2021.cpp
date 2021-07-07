fe13af8ef6a9ac92cb8541dc22414ba2a7ea69fc
Alexandr Maschenko <sany_26_93@icloud.com>
2021-07-07 21:04:38 +0300

    Исключения

---
+++ KlassWorkStep07.07.2021/KlassWorkStep07.07.2021/KlassWorkStep07.07.2021.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <sstream>
+using namespace std;
+
+
+int main()
+{
+	try {
+		string value = "6516g514";
+		int res = stoi(value);
+
+		cout << res << endl;
+	}
+	catch (exception a)
+	{
+		cout << "Invalid_argument: " << a.what() << endl;
+	}
+	
+}
